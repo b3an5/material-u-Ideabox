7447559f18685caf47b1b1d955980bd9c9fad001
B3an5 <beantheo@gmail.com>
2019-09-26 11:05:36 -0600

    added ideas to state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Controls from "./controls.js";
+import CardContainer from "./cardContainer.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Controls />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    ideas: []
+  };
+
+  submitCard = idea => {
+    let card = { ...idea, id: Date.now() };
+
+    this.setState({
+      ideas: [...this.state.ideas, card]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Controls submitCard={this.submitCard} />
+        <CardContainer ideas={this.state.ideas} />
+      </div>
+    );
+  }
 }
-
-export default App;
