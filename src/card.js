300cb446c90d33abee2d99863a9c4cff6015f56b
B3an5 <beantheo@gmail.com>
2019-09-30 14:16:19 -0600

    made card into paper ui comonents

--- src/card.js
+++ src/card.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
 
 export default function Card({ title, body, id }) {
+  const classes = useStyles();
   return (
     <div>
-      <h1>{title}</h1>
-      <h1>{body}</h1>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          {title}
+        </Typography>
+        <Typography component="p">{body}</Typography>
+      </Paper>
     </div>
   );
 }
