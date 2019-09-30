34c80654bc773d1eb18f3729ff967d029e1f9523
B3an5 <beantheo@gmail.com>
2019-09-30 14:16:38 -0600

    converted div into container ui component

--- src/cardContainer.js
+++ src/cardContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "./card.js";
+import Container from "@material-ui/core/Container";
 
 export default function CardContainer(props) {
   return (
-    <div>
+    <Container maxWidth="sm">
       {props.ideas.map((idea, key) => {
         return <Card {...idea} key={idea.id} />;
       })}
-    </div>
+    </Container>
   );
 }
